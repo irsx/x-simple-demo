a4d2caed0310d349b2317678ee0d9dcc76300bb9
panjipramana <panji@aviana.co.id>
2023-08-17 08:25:04 +0800

    ok

--- app.js
+++ app.js
@@ -3,10 +3,10 @@ const app = express()
 app.use(express.json())
 const product = require('./produk')
 require('dotenv').config()
-var https = require('https')
-let fs = require('fs')
+//var https = require('https')
+//let fs = require('fs')
 
-app.get('/products', async (req, res) => {
+app.get('/xsimple/products', async (req, res) => {
   res.json(product)
 })
 
@@ -14,11 +14,11 @@ app.listen(process.env.PORT, () => {
   console.log('service started:' + process.env.PORT)
 })
 
-var privateKey = fs.readFileSync('./ssl/server.key', 'utf8')
-var certificate = fs.readFileSync('./ssl/server.crt', 'utf8')
-var ca = fs.readFileSync('./ssl/server.ca', 'utf8')
-var credentials = { key: privateKey, cert: certificate, ca: ca }
-var httpsServer = https.createServer(credentials, app)
-httpsServer.listen(process.env.PORT, () => {
-  console.log('ISimple Demo started:' + process.env.PORT)
-})
+//var privateKey = fs.readFileSync('./ssl/server.key', 'utf8')
+//var certificate = fs.readFileSync('./ssl/server.crt', 'utf8')
+//var ca = fs.readFileSync('./ssl/server.ca', 'utf8')
+//var credentials = { key: privateKey, cert: certificate, ca: ca }
+//var httpsServer = https.createServer(credentials, app)
+// httpsServer.listen(process.env.PORT, () => {
+//  console.log('ISimple Demo started:' + process.env.PORT)
+//})
